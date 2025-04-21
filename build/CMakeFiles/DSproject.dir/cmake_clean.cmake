367beff80796dcde1afc9482f02fa8bba5641a80
Seif-3mk-hassan <seif253hassan@gmail.com>
2025-04-21 13:26:38 +0200

    GGGGGG

---
+++ build/CMakeFiles/DSproject.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DSproject.dir/main.cpp.obj"
+  "CMakeFiles/DSproject.dir/main.cpp.obj.d"
+  "DSproject.exe"
+  "DSproject.exe.manifest"
+  "DSproject.pdb"
+  "libDSproject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DSproject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -23,39 +23,7 @@ using namespace std;
 #define vi vector<int>
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    int n, m;
-    cin >> n >> m;
-    vector<int> a(n), b(m);
-    for (int i = 0; i < n; i++) {
-        cin >> a[i];
-    }
-    for (int i = 0; i < m; i++) {
-        cin >> b[i];
-    }
-
-    // Sort both arrays
-    sort(a.begin(), a.end());
-    sort(b.begin(), b.end());
-
-    int i = 0, j = 0;
-    int ans = LLONG_MAX;
-
-    // Two-pointer technique
-    while (i < n && j < m) {
-        ans = min(ans, abs(a[i] - b[j]));
-        if (a[i] < b[j]) {
-            i++;
-        } else {
-            j++;
-        }
-    }
-
-    cout << ans << endl;
-
+    
     return 0;
 }
 // This code finds the minimum absolute difference between elements of two arrays a and b.
