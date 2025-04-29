1e331b002d4aef33c0954c1ad853b88c1cf36154
Seif-3mk-hassan <seif253hassan@gmail.com>
2025-04-29 18:18:29 +0300

    f

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@
 #include<stack>
 #include<deque>
 #include<string>
-
+#include<lib/json.hpp>
 using namespace std;
 
 #define ll long long
@@ -21,12 +21,35 @@ using namespace std;
 #define ld long double
 #define vi vector<ll>
 
+
+using json = nlohmann::json;
+
+struct Node {
+    string id;
+    string name;
+    string pass;
+    vector<string> friends;
+    stack<string> messages;
+};
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    cout << "Hello World" << endl;
+    unordered_map<string, Node> umap;
+
+    json j;
+    for (int i = 0; i < 3; i++)
+    {
+        Node d;
+        umap[to_string(i)]={d.id=to_string(i), d.name="seif "+to_string(i), d.pass="pass"+to_string(i),{},{}};
+    }
+    for (auto j:umap)
+    {
+        cout << j.first << " : " << j.second.name <<" "<< j.second.pass <<" "<<j.second.messages.size()<<" "<<j.second.friends.size()<<endl;
+    }
+    
+    //cout << "Hello World" << endl;
     
     return 0;
 }
